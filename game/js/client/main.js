abb1aaaaba5d571fe763b93944ff6485076da530
Jerome <jerome.renaux@gmail.com>
2017-04-27 14:25:42 +0200

    Fixed width

--- game/js/client/main.js
+++ game/js/client/main.js
@@ -1,6 +1,6 @@
 
-var width = Math.min(Math.max(window.screen.width,window.screen.height),750);
-var game = new Phaser.Game(width,400, Phaser.AUTO, document.getElementById('game'));
+//var width = Math.min(Math.max(window.screen.width,window.screen.height),750);
+var game = new Phaser.Game(750,400, Phaser.AUTO, document.getElementById('game'));
 game.state.add('Game',Game);
 game.state.start('Game');
 
